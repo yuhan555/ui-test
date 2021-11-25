b52502c90fde4951dfa61ef7d66fa8d715d55c20
amberwu <amberwu@i-mps.com>
2021-11-25 11:24:27 +0800

    新增 文字輸入/單選/日期/日期範圍/日期時間 元素草稿

---
+++ src/components/FBDateGroup.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="FBDateGroup">
+    <div class="label">{{ label }}</div>
+    <div class="GroupContent">
+      <div style="width: calc(50% - 10px);">
+        <el-date-picker v-model="value1" type="date" placeholder="請選擇" style="width: 100%" :clearable= false></el-date-picker>
+      </div>
+      <div style="width: 20px;">-</div>
+      <div style="width: calc(50% - 10px);">
+        <el-date-picker v-model="value2" type="date" placeholder="請選擇" style="width: 100%" :clearable= false></el-date-picker></div>
+    </div>
+    <div class="error-info" v-if="hasErr">
+      <svg
+        width="13px"
+        height="11px"
+        viewBox="0 0 13 11"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <title>Error</title>
+        <defs>
+          <path
+            d="M7.49988182,83.5263158 C7.82607222,83.5263158 8.09080646,83.7856842 8.09080646,84.1052632 C8.09080646,84.4248421 7.82607222,84.6842105 7.49988182,84.6842105 C7.17369141,84.6842105 6.90895717,84.4248421 6.90895717,84.1052632 C6.90895717,83.7856842 7.17369141,83.5263158 7.49988182,83.5263158 Z M7.49988182,79.4736842 C7.82607222,79.4736842 8.09080646,79.7330526 8.09080646,80.0526316 L8.09080646,80.0526316 L8.09080646,82.3684211 C8.09080646,82.688 7.82607222,82.9473684 7.49988182,82.9473684 C7.17369141,82.9473684 6.90895717,82.688 6.90895717,82.3684211 L6.90895717,82.3684211 L6.90895717,80.0526316 C6.90895717,79.7330526 7.17369141,79.4736842 7.49988182,79.4736842 Z M12.7382515,85.4675263 C12.6029298,85.702 12.3393774,85.8421053 12.0332784,85.8421053 L2.96613068,85.8421053 C2.66003172,85.8421053 2.39647932,85.702 2.26174851,85.4675263 C2.19792864,85.3575263 2.11401735,85.1334737 2.27179422,84.8787368 L6.80477716,77.5156842 C7.08073897,77.0675789 7.91867011,77.0675789 8.19463192,77.5156842 L12.7282058,84.8787368 C12.8853917,85.1334737 12.8020714,85.3575263 12.7382515,85.4675263 M13.7404597,84.2806842 L9.20688583,76.9170526 C8.85292197,76.3427368 8.21472336,76 7.49970454,76 C6.78468572,76 6.14648711,76.3427368 5.79311417,76.9170526 L1.25954031,84.2806842 C0.923304191,84.8266316 0.913258472,85.4831579 1.23235778,86.0366316 C1.57450315,86.6312105 2.23929337,87 2.96613068,87 L12.0332784,87 C12.7607066,87 13.4249059,86.6312105 13.7676422,86.0366316 C14.0867415,85.4831579 14.0766958,84.8266316 13.7404597,84.2806842"
+            id="path-1"
+          ></path>
+        </defs>
+        <g
+          id="Symbols"
+          stroke="none"
+          stroke-width="1"
+          fill="none"
+          fill-rule="evenodd"
+        >
+          <g
+            id="Phone/Input/Textfeild/Error"
+            transform="translate(-1.000000, -76.000000)"
+          >
+            <mask id="mask-2" fill="white">
+              <use xlink:href="#path-1"></use>
+            </mask>
+            <use
+              id="Error"
+              fill="#FF0000"
+              fill-rule="nonzero"
+              xlink:href="#path-1"
+            ></use>
+          </g>
+        </g></svg
+      >{{ errMsg }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, reactive, toRefs, onMounted } from "vue";
+export default defineComponent({
+  name: "FBDateGroup",
+  props: {
+    label: String,
+    errMsg: String,
+    
+  },
+
+  setup() {
+    const state = reactive({
+      disabledDate(time) {
+        return time.getTime() > Date.now();
+      },
+
+      value1: '',
+      value2: '',
+    })
+
+    onMounted(() => {
+      document.querySelector('.FBDateGroup .GroupContent .el-input:nth-of-type(1) span:nth-of-type(1) i').classList.remove("el-icon-date");
+      document.querySelector('.FBDateGroup .GroupContent .el-input:nth-of-type(1) span:nth-of-type(2) i').classList.add("el-icon-date");
+      
+    })
+
+    return {
+      text: ref(""),
+      hasErr: false,
+      ...toRefs(state),
+    };
+  },
+});
+</script>
+
+
+<style lang="scss">
+@mixin mobile {
+  @media (max-width: 375px) {
+    @content;
+  }
+}
+
+.FBDateGroup {
+  text-align: left;
+  margin-top: 24px;
+
+  .label {
+    color: #727272;
+    text-align: left;
+    font-size: 1rem;
+    font-weight: 500;
+
+    @include mobile {
+      font-size: 0.875rem;
+    }
+  }
+
+  .el-date-editor.el-input {
+    width: 100%;
+  }
+
+  .el-input__inner {
+    height: 50px;
+    line-height: 50px;
+    color: #535353;
+    background-color: #ffffff;
+    border-color: #cccccc;
+    border-radius: 0.5rem;
+    padding: 0 20px !important;
+    font-size: 1.125rem;
+    margin-top: 3px;
+
+    @include mobile {
+      font-size: 1rem;
+    }
+  }
+
+  .GroupContent {
+    width: 100%;
+    display: flex;
+    text-align: center;
+    line-height: 50px;
+  }
+
+  //日曆icon
+  .el-icon-date:before {
+    font-size: 1.25rem;
+    color: #535353;
+  }
+
+  .el-input__suffix {
+    right: 10px;
+    top: 8px;
+  }
+
+  .el-input__suffix-inner{
+      pointer-events: none;
+    }
+}
+
+//必填欄位
+.required {
+  .label:before {
+    content: "*";
+    color: #ff0000;
+    font-size: 1.2rem;
+  }
+}
+
+//檢核錯誤
+.error {
+  .el-input__inner {
+    background-color: #fff7f6;
+    border: 1px solid #ff0000;
+  }
+
+  .error-info {
+    color: #ff0000;
+    font-size: 0.75rem;
+    font-weight: 500;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  svg {
+    padding: 3px 3px 0px 1px;
+  }
+
+}
+
+//經辦表單內的客戶欄位
+.customer {
+  .el-input__inner {
+    background-color: #fff9ef;
+  }
+}
+
+//測試用
+.FBDateGroup .GroupContent .el-input:nth-of-type(1) span:nth-of-type(1) i{
+  background-color: #ff0000;
+}
+.FBDateGroup .GroupContent .el-input:nth-of-type(1) span:nth-of-type(2) i{
+  background-color: #cada42;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <FBInput label="標題" errMsg="這是必填"/>
+    <FBSelect label="選擇" errMsg="這是必填"/>
+    <FBDate label="日期選擇" errMsg="這是必填"/>
+    <FBDateGroup label="日期群組" errMsg="這是必填"/>
+    <FBDateTime label="日期＆時間" errMsg="這是必填"/>
   </div>
 </template>
+
+<script>
+// @ 是/src的別名
+import FBInput from '@/components/FBInput.vue'
+import FBSelect from '@/components/FBSelect.vue'
+import FBDate from '@/components/FBDate.vue'
+import FBDateGroup from '@/components/FBDateGroup.vue'
+import FBDateTime from '@/components/FBDateTime.vue'
+
+console.log(FBInput)
+
+export default {
+  name: 'About',
+  //區域型元件在上層元件裡定義
+  components: {
+    FBInput,FBSelect,FBDate,FBDateGroup,FBDateTime
+  }
+}
+</script>
+
+
