679217da5a2e150d1165f09bf6bfadc0317cc5b6
amberwu <amberwu@i-mps.com>
2021-12-08 09:32:28 +0800

    新增 車牌辨識、單時間 元素

---
+++ src/components/FBTime.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="FBTime">
+    <div class="label">{{ label }}</div>
+    <el-time-select
+      v-model="value"
+      start="08:00"
+      step="00:60"
+      end="24:00"
+      placeholder="請選擇時間"
+      :clearable="false"
+    ></el-time-select>
+    <div class="error-info" v-if="hasErr">
+      <img class="icon-error" src="../images/icon-error.svg" />
+      {{ errMsg }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FBTime",
+  props: {
+    label: String,
+    errMsg: String,
+    data: {
+      type: Object,
+      default() {
+        return {
+          col: 12,
+          fieldType: null,
+        };
+      },
+    },
+  },
+
+  setup() {
+    return {
+      hasErr: false,
+    };
+  },
+
+  data() {
+    return {
+      value: "",
+    };
+  },
+});
+</script>
+
+
+<style lang="scss">
+@mixin mobile {
+  @media (max-width: 390px) {
+    @content;
+  }
+}
+
+.FBTime {
+  text-align: left;
+  margin-top: 24px;
+
+  .el-select {
+    width: 100%;
+  }
+
+  //下拉icon 位置
+  .el-input__suffix {
+    right: 10px;
+    top: 5px;
+  }
+
+  //關閉時鐘icon
+  .el-input__prefix {
+    display: none;
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,8 @@
     <FBInput label="標題" errMsg="這是必填"/>
     <FBSelect label="選擇" errMsg="這是必填"/>
     <FBDate label="日期選擇" errMsg="這是必填"/>
+    <FBTime label="時間選擇" errMsg="這是必填"/>
+
     <FBDateGroup label="日期群組" errMsg="這是必填"/>
     <FBDateTime label="日期＆時間" errMsg="這是必填"/>
 
@@ -11,6 +13,7 @@
     <FBRadioButton label="單選按鈕" errMsg="這是必填"/>
     <FBCheckBox label="多選框" errMsg="這是必填"/>
 
+    <FBCar label="車牌號碼" errMsg="這是必填"/>
     <FBOCR label="OCR-元件" errMsg="這是必填"/>
     <FBMail label="信箱" errMsg="這是必填"/>
     <FBCall label="市話" errMsg="這是必填"/>
@@ -25,11 +28,16 @@
 import FBInput from '@/components/FBInput.vue'
 import FBSelect from '@/components/FBSelect.vue'
 import FBDate from '@/components/FBDate.vue'
+import FBTime from '@/components/FBTime.vue'
+
 import FBDateGroup from '@/components/FBDateGroup.vue'
 import FBDateTime from '@/components/FBDateTime.vue'
+
 import FBRadioBox from '@/components/FBRadioBox.vue'
 import FBRadioButton from '@/components/FBRadioButton.vue'
 import FBCheckBox from '@/components/FBCheckBox.vue'
+
+import FBCar from '@/components/FBCar.vue'
 import FBOCR from '@/components/FBOCR.vue'
 import FBMail from '@/components/FBMail.vue'
 import FBCall from '@/components/FBCall.vue'
@@ -43,9 +51,9 @@ export default {
   name: 'About',
   //區域型元件在上層元件裡定義
   components: {
-    FBInput,FBSelect,FBDate,FBDateGroup,FBDateTime,
+    FBInput,FBSelect,FBDate,FBTime,FBDateGroup,FBDateTime,
     FBRadioBox, FBRadioButton, FBCheckBox,
-    FBOCR, FBMail,FBCall,FBAdress,
+    FBCar, FBOCR, FBMail,FBCall,FBAdress,
     FBButtons
   }
 }
